10cc519b82cb5ccfab669ae9f9e0f3602407efa2
DarkWorldCoder <ayushniroula08@gmail.com>
2023-05-01 22:31:39 +0545

    Java[0]

---
+++ java[0]/main/main.java
@@ -0,0 +1,30 @@
+package main;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class main{
+    public static void main(String[] args){
+        // String name = JOptionPane.showInputDialog("Enter name");
+        // JOptionPane.showMessageDialog(null,"Hello"+name);
+        // int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        // JOptionPane.showMessageDialog(null,"YOu are"+age+ "year old");
+
+    //  Scanner scanner = new Scanner(System.in);
+    //  System.out.println("What is your name?");
+    //  String name = scanner.nextLine();
+    //  System.out.println("What is your age?");
+
+    //  int age = scanner.nextInt();
+    //  System.out.println("Hello"+name+"your age is"+ age); 
+    // double x,y,z;
+    // Scanner scanner = new Scanner(System.in);
+    // System.out.println("Enter the side of x");
+    // x =scanner.nextDouble();
+    // System.out.println("Enter the side of y");
+    // y = scanner.nextDouble();
+    // z = Math.sqrt((x*x)+(y*y));
+    // System.out.println("The hypotenuse is"+z);
+    // scanner.close();
+
+
+    }
+}
